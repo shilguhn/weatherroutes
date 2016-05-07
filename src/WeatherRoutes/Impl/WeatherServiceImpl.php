fdfff09f3f5848d9423c784e8f8303db7249e840
Martin Koch <martin@shackeluri.com>
2016-05-07 12:24:36 +0200

    WIP

--- src/WeatherRoutes/Impl/WeatherServiceImpl.php
+++ src/WeatherRoutes/Impl/WeatherServiceImpl.php
@@ -2,6 +2,7 @@
 
 namespace WeatherRoutes\Impl;
 
+use Carbon\Carbon;
 use WeatherRoutes\Api\WeatherService;
 use WebX\Db\Db;
 use WebX\Db\DbException;
@@ -18,24 +19,23 @@ class WeatherServiceImpl implements WeatherService{
     {
         $inside = null;
         $outside = null;
+        $timestamp = null;
         try{
-            if($row = $this->db->firstRow("select * from archive_day_inTemp order by dateTime desc")){
-                $inside =  $this->fahrenheitToCelcius($row->float("min"));
+            if($row = $this->db->firstRow("select inTemp, outTemp, dateTime from archive order by dateTime desc")){
+                $inside =  $this->fahrenheitToCelcius($row->float("inTemp"));
+                $outside = $this->fahrenheitToCelcius($row->float("outTemp"));
+                $timestamp = Carbon::createFromTimestamp($row->int("dateTime"));
+
             }   
         } catch(DbException $e){
             var_dump($e);
         }
 
-        try{
-            if($row = $this->db->firstRow("select * from archive_day_outTemp order by dateTime desc")){
-                $outside =  $this->fahrenheitToCelcius($row->float("min"));
-            }
-        } catch(DbException $e){
-            return [null, null];
-        }
         return [
             "inside" => $inside,
-            "outside" => $outside
+            "outside" => $outside,
+            "timestamp" => $timestamp,
+            "humanTimestamp" => $timestamp->diffForHumans()
         ];
     }
 
